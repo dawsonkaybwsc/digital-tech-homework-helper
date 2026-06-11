82d7f54e0526feb22fb1e0d15fac5218af0b9dd8
dawsonkaybwsc <dawsonkayb@wsc.school.nz>
2026-06-12 09:42:58 +1200

    ygiy

--- HomeworkHelper/HomeworkHelper/obj/Debug/net10.0-windows/HomeworkHelper.AssemblyInfo.cs
+++ HomeworkHelper/HomeworkHelper/obj/Debug/net10.0-windows/HomeworkHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeworkHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab128b357f6deb9f9ff371e4f06622d8986dbe71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4023eaf1aef87d5e91dea64514f754519edcefb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeworkHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeworkHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
